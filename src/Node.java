396c387c9cd16e14326d4f032b01f66637f0a60b
William Writer <williamwriter@Williams-MacBook-Pro-2.local>
2021-10-03 21:03:45 -0400

    Fixed up formatting to be more general and finished adding/fixing methods

--- src/Node.java
+++ src/Node.java
@@ -1,38 +1,65 @@
+import java.util.Objects;
+
+/**
+ * Node Practice
+ * Just create a node class
+ * Author: William Writer
+ * Collaborator(s): Jake(my brother)
+ * Collaboration: He said that my variables were terrible so I changed them
+ * Date: Date the program is handed in
+ * On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: WW
+ **/
 public class Node<T> {
-    private T t;
-    private Node dog;
-    private Node puppy;
+    private T data;
+    private Node<T> prev;
+    private Node<T> next;
 
     public Node(){
+        prev = null;
+        next = null;
     }
     public Node(T t){
-        this.t =t;
+        this.data =t;
     }
-    public Node(T t, Node dog, Node puppy){
-        this.t =t;
-        this.dog = dog;
-        this.puppy = puppy;
+    public Node(T data, Node<T> prev, Node<T> next){
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
     }
-    public T get_t(){
-        return t;
+    public T get_data(){
+        return data;
     }
-    public Node get_dog(){
-        return dog;
+    public Node<T> getPrev(){
+        return prev;
     }
-    public Node get_puppy(){
-        return puppy;
+    public Node<T> getNext(){
+        return next;
     }
-    public T set_t(T t){
-        return this.t = t;
+    public T set_data(T data){
+        return this.data = data;
     }
-    public Node set_dog(Node dog){
-        return this.dog = dog;
+    public Node<T> set_dog(Node<T> prev){
+        return this.prev = prev;
     }
-    public Node set_puppy(Node puppy){
-        return this.puppy = puppy;
+    public Node<T> set_next(Node<T> next){
+        return this.next = next;
     }
 
     public String toString(){
-        return "Node: ";
+        return "Node: "+ data;
+    }
+
+    @Override // Inteli J did this for me, but it is overriding the .equals method
+    public boolean equals(Object o) {
+        if (this == o) return true; // if they have the same memory location
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(data, node.data) &&
+                Objects.equals(prev, node.prev) &&
+                Objects.equals(next, node.next);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, prev, next);
     }
 }
